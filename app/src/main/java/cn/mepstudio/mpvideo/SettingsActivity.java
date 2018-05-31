c0f69f1452f3aee55c351e9533e481347e6b9080
DoonZhang <DoonZhang@qq.com>
2018-05-31 15:01:20 +0800

    添加几个页面

--- app/src/main/java/cn/mepstudio/mpvideo/SettingsActivity.java
+++ app/src/main/java/cn/mepstudio/mpvideo/SettingsActivity.java
@@ -3,11 +3,11 @@ package cn.mepstudio.mpvideo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class SplashActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_settings);
     }
 }
