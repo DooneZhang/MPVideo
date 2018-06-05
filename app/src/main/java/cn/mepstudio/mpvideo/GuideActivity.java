3b42681be04185e40973f5e6dae7566fc4f56c65
DoonZhang <DoonZhang@qq.com>
2018-06-05 23:59:19 +0800

    添加首次安装引导页面

--- app/src/main/java/cn/mepstudio/mpvideo/GuideActivity.java
+++ app/src/main/java/cn/mepstudio/mpvideo/GuideActivity.java
@@ -3,11 +3,11 @@ package cn.mepstudio.mpvideo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class AboutActivity extends AppCompatActivity {
+public class GuideActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
+        setContentView(R.layout.activity_guide);
     }
 }
